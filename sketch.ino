2bc87a1ab9cc06132a12d6588ec728d1c90b9455
sharmilajeevitha208-arch <sharmilajeevitha208@gmail.com>
2026-07-26 10:52:03 +0530

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,26 @@
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+    dht.begin();
+    }
+
+    void loop() {
+      float temperature = dht.readTemperature();
+        float humidity = dht.readHumidity();
+
+          Serial.print("Temperature: ");
+            Serial.print(temperature);
+              Serial.print(" °C  ");
+
+                Serial.print("Humidity: ");
+                  Serial.print(humidity);
+                    Serial.println(" %");
+
+                      delay(2000);
+                      }
